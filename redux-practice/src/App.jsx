610ef4a32d14abeb2cb6b8c5d1359cf85576f79b
tyson lind <tysonfloydlind@gmail.com>
2024-09-28 01:32:46 -0600

    removed base vite app readme and unused imports to clean up the project

--- redux-practice/src/App.jsx
+++ redux-practice/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { addTask, removeTask } from './firstSlice.js'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 
